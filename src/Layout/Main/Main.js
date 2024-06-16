ed193ff9fdb5ed2981ad957fd5b9aef1c2372052
mhsetu <mhsetu111@gmail.com>
2024-06-16 10:29:16 +0600

    second

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Header from '../../Pages/Shared/Header/Header';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
-    return (
+  return (
+    <div>
+      <Header></Header>
+      <div>
+       <div >
+        <h1>Advertisement</h1>
+        </div> 
         <div>
-        
+            <Outlet></Outlet>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,16 @@
-const { createBrowserRouter } = require('react-router-dom');
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../Layout/Main/Main';
+import Home from '../../Pages/Home/Home/Home';
 
-const router = createBrowserRouter([{
-    path:'/',
-    element:
-}]);
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
